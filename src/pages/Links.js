88a2191ca5f531cf8b4adfd8418de9c2b40963dd
Zachary Johnson <vepped@gmail.com>
2023-07-06 14:36:47 -0400

    Resume Page
    Got a copy of my most revised version of my resume up on the site! I used a module called React-pdf which had some side effects with it that took some testing and reading of the docs. I figured out also how to make a download able link which was pretty cool!

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -1,3 +1,5 @@
+
+
 function Links () {
     return(
         <div>
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,9 +1,35 @@
+import { Document,Page,pdfjs} from "react-pdf"
+import React from "react"
+import resume from "../PDFs/resume.pdf"
+
+
+
+
 function Resume () {
+    pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+        'pdfjs-dist/build/pdf.worker.min.js',
+        import.meta.url,
+      ).toString();
+
     return(
-        <div>
+        <div className="Resume">
             <h2>
-                Here is a link to my most recent Resume
+                Here is a <a href={resume} download={"Zachary Johnson Resume"}>link</a> to download my most recent Resume
             </h2>
+
+            <Document file={resume} className={"resumePDF"}>
+                <Page pageNumber={1}
+                renderTextLayer={false}
+                renderAnnotationLayer={false}
+                scale={2}
+                />
+                <Page pageNumber={2}
+                renderTextLayer={false}
+                renderAnnotationLayer={false}
+                scale={2}
+                />
+                
+            </Document>
         </div>
     )
 }
