bbea2c22a71257e98a4a7402c88e6c5472e3690f
Zachary Johnson <vepped@gmail.com>
2025-12-29 21:48:46 -0600

    Walking in multiple directions
    With the help of multiple AI's I was able to create buttons, and use them to influence the direction of the walker. I want to add a button that just sets it to neutral now too

--- src/components/Walker.js
+++ src/components/Walker.js
@@ -1,6 +1,5 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import p5 from 'p5'
-
 
 
 class RandomWalk extends React.Component{
@@ -8,24 +7,33 @@ class RandomWalk extends React.Component{
         super()
         this.myRef = React.createRef()
     }
-
-
+    
+    
     Sketch = (p) =>{
         let walker
-
+        let outcomeText
+        
         class Walker {
             constructor() {
+                this.probability = [.25,.25,.25,.25]
                 this.x = p.width / 2;
                 this.y = p.height / 2;
             };
         
+
+            setProbability(newProb) {
+                if (!Array.isArray(newProb) || newProb.length !== 4) return;
+                const sum = newProb.reduce((a,b)=>a+b,0);
+                if (sum <= 0) return;
+                this.probability = newProb.map(v => v / sum); // normalize
+    }
             step() {
-                    const choice = Math.floor(Math.random() * 4);
-                if (choice === 0) {
+                    const choice = Math.random(1);
+                if (choice < (this.probability[0])) {
                 this.x++;
-                } else if (choice === 1) {
+                } else if (choice < (this.probability[0]+this.probability[1])) {
                 this.x--;
-                } else if (choice === 2) {
+                } else if (choice < (this.probability[0]+this.probability[1]+this.probability[2])) {
                 this.y++;
                 } else {
                 this.y--;
@@ -38,6 +46,41 @@ class RandomWalk extends React.Component{
             };
         }
         p.setup =()=>{
+
+              let centerX = p.windowWidth / 2;
+              let bottomY = p.windowHeight - 80; 
+              let spacing = 40; 
+              let upButton = p.createButton('▲');
+                upButton.position(centerX - 15, bottomY - spacing);
+                upButton.mousePressed(() => {
+                    outcomeText = "Moving Up!";
+                    if (walker) walker.setProbability([.2,.2,.2,.4])
+                });
+
+                // Down Button
+                let downButton = p.createButton('▼');
+                downButton.position(centerX - 15, bottomY);
+                downButton.mousePressed(() => {
+                    outcomeText = "Moving Down!";
+                    if (walker) walker.setProbability([.2,.2,.4,.2])
+                });
+
+                // Left Button
+                let leftButton = p.createButton('◄');
+                leftButton.position(centerX - 15 - spacing, bottomY);
+                leftButton.mousePressed(() => {
+                    outcomeText = "Moving Left!";
+                    if (walker) walker.setProbability([.2,.4,.2,.2]);
+                });
+
+                // Right Button
+                let rightButton = p.createButton('►');
+                rightButton.position(centerX - 15 + spacing, bottomY);
+                rightButton.mousePressed(() => {
+                    outcomeText = "Moving Right!";
+                    if (walker) walker.setProbability([.4,.2,.2,.2]);
+                });
+                
             let canvas = p.createCanvas(p.windowWidth, p.windowHeight)
             canvas.position(0, 0)
             canvas.style("z-index", "-1")
