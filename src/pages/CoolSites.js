312399234d891f0548af4f6f3e6ece3033e17ac9
zacamz <vepped@gmail.com>
2026-09-20 07:13:20 -0500

    mo CoolSites fo sho

--- src/pages/CoolSites.js
+++ src/pages/CoolSites.js
@@ -2,7 +2,7 @@ function CoolSites() {
     return (
         <div className="CoolSites text-page">
             <h2>
-                Cool Sites that I have Stumbled Upon. Hopefully they Pique your Intrest!
+                Cool Sites that I have Stumbled Upon. Hopefully they piques your interest!
             </h2>
             <p>
                 <a href="https://www.sive.rs">siv.rs</a>: Derek Sivers is an awesome guy I have listened to his books and Love his <a href="https://www.nownownow.com">NowNowNow</a> Site which is how I have found most of the other people on this Page probably
@@ -10,6 +10,9 @@ function CoolSites() {
             <p>
                 <a href="https://www.cassidoo.co">cassidoo.co</a>: Cassidy Williams is another person that just really inspires me to continue to be creative with code! Subscribe to her <a href="https://cassidoo.co/newsletter">Newsletter</a> for links to great articles about coding, tech, a coding challenge, things that are just good in life and a joke you try not to smile at!
             </p>
+            <p><a href="https://thecodingtrain.com/">thecodingtrain.com</a>: Daniel Shiffman is an awesome teacher and Art/Nature/Math mixed with Coding wizard. Seriously an Inspiration! Check out his <a href="https://www.youtube.com/watch?v=O5wjXoFrau4&t=6s">YouTube</a> and <a href="https://natureofcode.com/">Book</a>!</p>
+            <p><a href="https://www.drew.soy">drew.soy</a>: Drew is the first really interesting person I have found on <a href="https://www.Nownownow.com">Nownownow</a> and I love his <a href="https://www.drew.soy/disconnections">disconnections</a> idea for a game! </p>
+            <p><a href="https://www.williamjansson.com">williamjansson.com</a>: William Jansson is another cool blog I found through Nownownow technically, through <a href="https://www.ecorpuz.com">ecorpubz.com</a> really aprreciate the simplicity and honesty and the intro to PGP</p>
             {/* <p><a href="https://www"></a>: </p> */}
             {/* <p><a href="https://www"></a>: </p> */}
         </div>
